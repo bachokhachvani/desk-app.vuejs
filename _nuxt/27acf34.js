19c9343b0bb769d6b529f4f98d37c73fc5279e83
bachokhachvani <khachvanis@gmail.com>
2022-12-09 02:04:02 +0400

    851ece3

---
+++ _nuxt/27acf34.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{375:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},c=t(58),component=Object(c.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
